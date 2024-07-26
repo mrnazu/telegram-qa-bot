5c454713f4e55147ded615c12e7559973f852c47
mrnazu <samuelamsalu1998@gmail.com>
2024-07-26 03:41:58 -0700

    feat: apiUrl in config.js from localhost to vercel.app

--- config/config.js
+++ config/config.js
@@ -3,5 +3,5 @@ require('dotenv').config();
 module.exports = {
     token: process.env.TELEGRAM_BOT_TOKEN,
     channelUsername: process.env.TELEGRAM_CHANNEL_USERNAME,
-    apiUrl: 'http://localhost:3000/api/creeds/westminster_larger_catechism/questions/'
+    apiUrl: 'https://reformed-api-erka.vercel.app/api/creeds/westminster_larger_catechism/questions/'
 };
