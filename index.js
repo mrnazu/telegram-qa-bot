0d3816cba0d3c1660d1bcb89eea6f083f107d9b1
mrnazu <samuelamsalu1998@gmail.com>
2024-07-26 18:41:57 -0700

    Added MongoDB connection and updated index.js to call postDailyQuestion after connection establishment

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+// index.js
+
+const { connectToMongoDB } = require('./utils/mongodb');
 const { postDailyQuestion } = require('./cron');
 
-// This line is optional if I want to trigger postDailyQuestion manually for testing.
-// postDailyQuestion(); 
-
-console.log('Index file running. Cron job should be scheduled.');
+// Connect to MongoDB
+connectToMongoDB().then(() => {
+    console.log('MongoDB connection established successfully.');
+    postDailyQuestion();
+}).catch(err => {
+    console.error('Error establishing MongoDB connection:', err);
+});
